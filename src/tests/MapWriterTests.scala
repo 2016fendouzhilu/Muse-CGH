25202942641214304ce359ac7570602b2a4f5ae4
魏家一 <wjydzh1@163.com>
2016-03-03 15:22:22 +0800

    MapWriter finished

---
+++ src/tests/MapWriterTests.scala
@@ -0,0 +1,70 @@
+package tests
+
+import editor.Editing
+import main.{Letter, LetterSeg}
+import utilities.Parsing._
+import utilities.{CubicCurve, Vec2}
+import org.scalacheck.Prop._
+import org.scalacheck.Gen
+import org.scalacheck.Arbitrary.arbitrary
+
+/**
+  * Created by weijiayi on 3/3/16.
+  */
+object MapWriterTests {
+
+  import utilities.MapWriter._
+
+  val vec2Gen = for{
+    x <- arbitrary[Double]
+    y <- arbitrary[Double]
+  } yield Vec2(x,y)
+
+  val cubicGen = for{
+    p0 <- vec2Gen
+    p1 <- vec2Gen
+    p2 <- vec2Gen
+    p3 <- vec2Gen
+  } yield CubicCurve(p0,p1,p2,p3)
+
+  val letterSegGen = for{
+    c <- cubicGen
+    dots <- Gen.choose(1, 100)
+    start <- Gen.choose(0.01, 1.0)
+    end <- Gen.choose(0.01, 1.0)
+    b1 <- arbitrary[Boolean]
+    b2 <- arbitrary[Boolean]
+  } yield LetterSeg(c, dots, start, end, b1, b2)
+
+  val letterGen = for{
+    segs <- Gen.containerOf[IndexedSeq, LetterSeg](letterSegGen)
+    w <- Gen.choose(0.2,1.5)
+    t <- Gen.choose(0.2,1.5)
+    d <- Gen.choose(0.2,1.5)
+  } yield Letter(segs, w, t, d)
+
+  val editingGen = for{
+    l <- letterGen
+    selects <- Gen.containerOf[List, Int](Gen.choose(0,100))
+  } yield Editing(l, selects)
+
+  val vec2Check = forAll(vec2Gen){ v => readOption[Vec2](write(v)) contains v }
+
+  val cubicCheck = forAll(cubicGen){ c => readOption[CubicCurve](write(c)) contains c }
+
+  val letterSegCheck = forAll(letterSegGen) { l => readOption[LetterSeg](write(l)) contains l}
+
+  val letterCheck = forAll(letterGen) { l => readOption[Letter](write(l)) contains l}
+
+  val editingCheck = forAll(editingGen) { e => readOption[Editing](write(e)) contains e}
+
+
+  def main(args: Array[String]) {
+    vec2Check.check
+    cubicCheck.check
+    letterSegCheck.check
+    letterCheck.check
+    editingCheck.check
+  }
+
+}
---
+++ src/utilities/MapWriter.scala
@@ -0,0 +1,102 @@
+package utilities
+
+import editor.Editing
+import main.{Letter, LetterSeg}
+import utilities.MapWriter.MapData
+
+/**
+  * Created by weijiayi on 3/3/16.
+  */
+trait MapWriter[A] {
+  def toMapData(v: A): MapData
+
+  def fromMapData(data: MapData): A
+}
+
+object MapWriter {
+  type MapData = Map[MapKey.Value, Any]
+
+  def write[A](value: A)(implicit mapWriter: MapWriter[A]) = mapWriter.toMapData(value)
+
+  def readOption[A](data: MapData)(implicit mapWriter: MapWriter[A]): Option[A] = {
+    try{
+      Some(mapWriter.fromMapData(data))
+    } catch{
+      case e: Throwable => None
+    }
+  }
+
+  import MapKey._
+
+  implicit val Vec2MapWriter = new MapWriter[Vec2] {
+    override def toMapData(v: Vec2): MapData = Map((Vec2X, v.x),(Vec2Y, v.y))
+
+    override def fromMapData(data: MapData): Vec2 = {
+      Vec2(data(Vec2X).asInstanceOf[Double], data(Vec2Y).asInstanceOf[Double])
+    }
+  }
+
+  implicit val CubicCurveWriter = new MapWriter[CubicCurve] {
+    override def toMapData(v: CubicCurve): MapData = Map(
+      (P0, v.p0), (P1, v.p1), (P2, v.p2), (P3, v.p3)
+    )
+
+    override def fromMapData(data: MapData): CubicCurve = CubicCurve(
+      data(P0).asInstanceOf[Vec2],
+      data(P1).asInstanceOf[Vec2],
+      data(P2).asInstanceOf[Vec2],
+      data(P3).asInstanceOf[Vec2]
+    )
+  }
+
+  implicit val LetterSegWriter = new MapWriter[LetterSeg] {
+    override def toMapData(v: LetterSeg): MapData = v match{
+      case LetterSeg(curve, dots, start, end, align, isBreak) => Map(
+        (Curve, curve), (Dots, dots), (StartWidth, start), (EndWidth, end),
+        (AlignTangent, align), (IsStrokeBreak, isBreak)
+      )
+    }
+
+    override def fromMapData(data: MapData): LetterSeg = LetterSeg(
+      data(Curve).asInstanceOf[CubicCurve], data(Dots).asInstanceOf[Int],
+      data(StartWidth).asInstanceOf[Double], data(EndWidth).asInstanceOf[Double],
+      data(AlignTangent).asInstanceOf[Boolean], data(IsStrokeBreak).asInstanceOf[Boolean]
+    )
+  }
+
+  implicit val LetterWriter = new MapWriter[Letter] {
+    override def toMapData(v: Letter): MapData = v match {
+      case Letter(segs, w, t, d) => Map(
+        (Segs, segs.toVector), (Width, w), (Tall, t), (Deep, d)
+      )
+    }
+
+    override def fromMapData(data: MapData): Letter = Letter(
+      data(Segs).asInstanceOf[Vector[LetterSeg]],
+      data(Width).asInstanceOf[Double],
+      data(Tall).asInstanceOf[Double],
+      data(Deep).asInstanceOf[Double]
+    )
+  }
+
+  implicit val EditingWriter = new MapWriter[Editing] {
+    override def toMapData(v: Editing): MapData = v match{
+      case Editing(l, selects) => Map(
+        (EditingLetter, l), (EditingSelects, selects.toList)
+      )
+    }
+
+    override def fromMapData(data: MapData): Editing = Editing(
+      data(EditingLetter).asInstanceOf[Letter],
+      data(EditingSelects).asInstanceOf[List[Int]]
+    )
+  }
+}
+
+object MapKey extends Enumeration{
+  val Vec2X, Vec2Y = Value
+  val P0, P1, P2, P3 = Value
+  val Curve, Dots, StartWidth, EndWidth, AlignTangent, IsStrokeBreak = Value
+  val Segs, Width, Tall, Deep = Value
+  val EditingLetter, EditingSelects = Value
+}
